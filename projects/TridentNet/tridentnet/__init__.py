cee0bdcccfd7a4813c45fee8e7c21fe9fbe877bf
huijie <wszhj2008@gmail.com>
2020-09-22 10:21:02 -0700

    atss init

--- projects/TridentNet/tridentnet/__init__.py
+++ projects/TridentNet/tridentnet/__init__.py
@@ -5,5 +5,9 @@ from .trident_backbone import (
     build_trident_resnet_backbone,
     make_trident_stage,
 )
+from .atss_subsample import atss_subsample
 from .trident_rpn import TridentRPN
+from .atss_rpn import ATSSRPN
 from .trident_rcnn import TridentRes5ROIHeads, TridentStandardROIHeads
+
+from .config import add_atss_config
